c99ed0ed10bec0e3f7398935d0faff9e964e156e
vicky <lch82327@gmail.com>
2016-04-30 21:52:23 +0800

    question and questionaire test

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :questions
+  test "question attributes must not be empty" do
+    question = Question.new
+    assert question.invalid?
+    assert question.errors[:questionaire_id].any?
+    assert question.errors[:content].any?
+  end
 end
--- test/models/questionaire_test.rb
+++ test/models/questionaire_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class QuestionaireTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :questionaires
+  test "questionaire attributes must not be empty" do
+    q = Questionaire.new
+    assert q.invalid?
+    assert q.errors[:user_id].any?
+    assert q.errors[:title].any?
+  end
 end
