af26f15d4f68178651a760c7942d97576831f38a
vicky <lch82327@gmail.com>
2016-04-30 10:08:53 +0800

    add relationship for Question and Choice

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160429020346) do
+ActiveRecord::Schema.define(version: 20160429155552) do
+
+  create_table "choices", force: :cascade do |t|
+    t.integer  "question_id"
+    t.text     "content"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.text     "content"
